6a6539b91a21d79bf1c5cc12a63c496fca099d61
Szymon <113582467+szymon02913@users.noreply.github.com>
2022-11-30 19:17:01 +0100

    Add files via upload

--- strona.php
+++ strona.php
@@ -3,11 +3,9 @@
         <style>
         .d1
         {
-            background: grey;
+            background: pink;
             height: 800px;
-            width: 500px;
-            border: 50%;
-            outline: 5px black solid;
+            width: 1200px;
             font-size: 20px;
             text-align: center;
         }
@@ -19,12 +17,37 @@
         {
             background: lightgrey;
         }
+        .srodek
+        {
+            margin: 15px 15px 15px 15px;
+            text-align: left;
+        }
         </style>
     </head>
     <body>
         <center><div class="d1">
     <?php
+        session_start();
+        echo 'Zalogowany jako: '.$_SESSION["login"].'';
 
+        echo '<form action="index.php"><button>Wyloguj</button></form>';
+        echo '<form action="dodawanie.php"><button>Wystaw</button></form>';
+
+        $con = new mysqli("127.0.0.1","root","","projekt_sklep");
+        echo '<form method="POST" action="dodawanie.php">';
+        $res = $con->query("SELECT * FROM offerts");
+        $cos = $res->fetch_all();
+
+        $res1 = $con->query("SELECT * FROM users");
+        $cos1 = $res1->fetch_all();
+
+        echo '<div class="srodek">';
+        for($i=0; $i<count($cos);$i++)
+        {
+            echo 'item: '.$cos[$i][1].', Sprzedający: '.$cos1[$cos[$i][3]][1].'<br>';
+        }
+
+        echo '</div></form>';
     ?>
         </div></center>
     </body>
