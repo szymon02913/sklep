89e50496a3e1bf89ee88a6d35873d3aa3aa11efe
Szymon <113582467+szymon02913@users.noreply.github.com>
2022-11-30 19:18:12 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,46 +1,51 @@
 <html>
     <head>
-    <title>OLX</title>
-    <style>
-        .header{
-        background-color:powderblue;
+        <style>
+        .d1
+        {
+            background: pink;
+            height: 800px;
+            width: 1200px;
+            font-size: 20px;
+            text-align: center;
         }
-    </style>
-    
-    
-    
+        input
+        {
+            background: lightgrey;
+        }
+        button
+        {
+            background: lightgrey;
+        }
+        </style>
     </head>
     <body>
-    <div class="header">
     <?php
-
+        session_start();
         $con = new mysqli("127.0.0.1","root","","projekt_sklep");
-        if(isset($_POST['login'])&&strlen($_POST['login'])){
-        }
-      
-            
-        $login="";
-        $password="";
-        $res=$con->query("SELECT * FROM users");
-        $offers=$res->fetch_all(MYSQLI_ASSOC);
-        $res->fetch_all();
-        echo"<center><b><h1>ZALOGUJ SIĘ</h1></b></center>";
-    ?>
-     <br>
+        echo '<form method="POST">';
+        $res = $con->query("SELECT * FROM users");
+        $cos = $res->fetch_all();
 
-    <center>
-    <form method="POST">
-        
-     Login:     <input type="text" name="login" placeholder="Podaj login"value="<?php echo $login;?>">
-     Password:  <input type="text" name="password" placeholder="Podaj hasło"value="<?php echo $password;?>">
-                <input type=submit value="ZALOGUJ">
-    </form>
-    </center>
-    
-   
-  
- 
-    
-    </div>
+        echo '<center><div class="d1"><h1>Logowanie:</h1><br> Login: <input name="login"><br> Haslo: <input name="password" type="password"><br><input type="submit">';
+        if($_POST!=NULL)
+        {
+            for($i=0;$i<count($cos);$i++)
+            {
+                if($_POST['login']==$cos[$i][1] && $_POST['password']==$cos[$i][2])
+                {
+                    $_SESSION["login"] = $_POST['login'];
+                    $_SESSION["id"] = $i;
+                    echo 'udalo sie zalogowac';
+                    header("Location: strona.php");
+                }
+            }
+        }
+        echo '</form>';
+    ?>
+    <?php
+        echo '<form action="rejestracja.php"><button>Rejestracja</button></form></center></div>';
+    ?>
+
     </body>
 </html>
