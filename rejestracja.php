6ab53b71d2af9ecceb02f205eccbb57c5004bbb9
Szymon <113582467+szymon02913@users.noreply.github.com>
2022-11-30 19:17:56 +0100

    Add files via upload

--- rejestracja.php
+++ rejestracja.php
@@ -1,28 +1,45 @@
-<html lang="pl">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
+<html>
+    <head>
+        <style>
+        .d1
+        {
+            background: pink;
+            height: 800px;
+            width: 1200px;
+            font-size: 20px;
+            text-align: center;
+        }
+        input
+        {
+            background: lightgrey;
+        }
+        button
+        {
+            background: lightgrey;
+        }
+        </style>
+    </head>
+    <body>
     <?php
-    $x = new mysqli('127.0.0.1', 'root', '', 'projekt_sklep');
-    ?>
-</head>
-<body>
-    <form method="POST">
-        Login:
-        <input type="text" name="user_name"><br>
-        Hasło:
-        <input type="password" name='password'><br>
-        <button type="submit">Zarejestruje się</button>   
-    </form>
+        $con = new mysqli("127.0.0.1","root","","projekt_sklep");
+        echo '<form method="POST">';
+        $res = $con->query("SELECT * FROM users");
+        $cos = $res->fetch_all();
 
-    <?php
-    if(isset($_POST["login"]) and isset($_POST['password'])) {
-        $y = "INSERT INTO `users`(`login`, `password`) VALUES ('".$_POST["login"]."','".$_POST["password"]."')";
-        $x->query($y);
-        $x->close();
-        header('Location: login.php');
-    }
-    $x->close();
+        echo '<center><div class="d1"><h2>Rejestracja:</h2><br> Login: <input name="login"><br> Haslo: <input name="password" type="password"><br><input type="submit">';
+        if($_POST!=NULL)
+        {
+            if($_POST['login']!="" && $_POST['password']!="")
+            {
+                $sqlquery = "INSERT INTO `users` VALUES ('".count($cos)."', '".$_POST['login']."', '".$_POST['password']."', '0', '');";
+                $con->query($sqlquery);
+                header('location: index.php');
+            }
+        }
+        echo '</form>';
+
+        echo '<form action="index.php"><button>Logowanie</button></form></center></div>';
     ?>
-</body>
+
+    </body>
 </html>
